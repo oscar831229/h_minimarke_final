ae30bb5c51937a12b70cf66c0ed50ede8330d6ef
root <carvajaldiazeduar@gmail.com>
2018-02-25 19:02:44 -0500

    save progress

--- schemas/to6112.sql
+++ schemas/to6112.sql
@@ -122,13 +122,6 @@ alter table movi_niif drop column createdTime;
 alter table movitempniif drop column createdTime;
 
 alter table inve add column rodizio char(1) null default 'N';
-<<<<<<< HEAD
-
-alter table movitemp drop primary key;
 
 alter table nits add column email varchar(200) null;
-alter table movi add column consecutivo int null;
-alter table movitemp add column consecutivo int null; 
-=======
 alter table movi drop column consecutivo;
->>>>>>> 33170694502e84e78a2d9909c4368eefb6c4d370
